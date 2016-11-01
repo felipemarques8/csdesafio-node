2971b6159fed3ca2da8b4454909f2d32f585b736
Felipe Marques <felipemarques@Felipes-MacBook-Pro.local>
2016-11-01 04:33:05 -0300

    Iniciando a aplicação MEAN

---
+++ README.md
@@ -0,0 +1,1 @@
+Desafio Node.js
---
+++ app.js
@@ -0,0 +1,38 @@
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('static-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var routes = require('./routes/index');
+var users  = require('./routes/users');
+
+var app = express();
+
+//middleware
+var erros = require('./middleware/erros');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({ secret: 'sua-chave-secreta' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+//middleware
+app.use(erros.notfound);
+app.use(erros.serverError);
+
+app.listen(3000, function() {
+    console.log('Express server listening on port 3000');
+});
